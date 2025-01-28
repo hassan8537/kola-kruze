f2c5b3b8d8b2e300b7eb7ddfa5a93eb2ae439a38
root <root@client1.appsstaging.com>
2025-01-28 15:49:05 +0000

    Remote changes

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.use(`/api/${process.env.API_VERSION}`, [
   userRoutes.protected
 ]);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 connectToDatabase()
   .then(() => {
