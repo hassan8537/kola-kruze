5cdf34e2bce4c8df17efff4fa3e3f3768de3b671
Muhammad Hassan Ahmed <hassan.ahmed@binatedigital.com>
2025-02-07 16:51:46 +0500

    Vehicle

--- src/services/user-services/category-service.js
+++ src/services/user-services/category-service.js
@@ -1,4 +1,4 @@
-const Category = require("../../models/category");
+const Category = require("../../models/Vehicle-Category");
 const { populateCategory } = require("../../populate/populate-models");
 const {
   errorResponse,
