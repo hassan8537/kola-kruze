76c7e95d0551eed5f63edd2e0d640027fc311605
Muhammad Hassan Ahmed <hassan.ahmed@binatedigital.com>
2025-02-18 16:14:24 +0500

    Instant Rides

--- src/services/user-services/ride-service.js
+++ src/services/user-services/ride-service.js
@@ -877,13 +877,11 @@ class Service {
     try {
       const { user_id, ride_id, cancellation } = data;
 
-      const ride = await this.ride
-        .findOne({
-          _id: ride_id,
-          $or: [{ user_id }, { driver_id: user_id }],
-          ride_status: { $in: ["accepted", "booked"] }
-        })
-        .populate(populateRide.populate);
+      const ride = await this.ride.findOne({
+        _id: ride_id,
+        $or: [{ user_id }, { driver_id: user_id }],
+        ride_status: { $in: ["accepted", "booked"] }
+      });
 
       if (!ride) {
         return socket.emit(
